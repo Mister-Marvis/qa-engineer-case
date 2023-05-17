41d442b60aafb0d901c2b21986e33b0fbd6ff723
Dick de Leeuw <dick@leeuw.studio>
2023-05-17 11:24:37 +0200

    Improved design.

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,24 +1,19 @@
-import {ChangeEventHandler, Dispatch, FunctionComponent, SetStateAction} from "react";
-import { TodoItem } from "@/types/todo";
+import { Dispatch, FunctionComponent, SetStateAction } from 'react'
+import { TodoItem } from '@/types/todo'
 
 interface ListItemProps {
-  item: TodoItem;
-  setTodoItems: Dispatch<SetStateAction<TodoItem[] | undefined>>;
-  index: number;
+  item: TodoItem
+  setTodoItems: Dispatch<SetStateAction<TodoItem[] | undefined>>
+  index: number
 }
 
-const TodoListItem: FunctionComponent<ListItemProps> = ({
-  item: { done, text },
-  setTodoItems,
-  index,
-}) => {
-  const handleChange: ChangeEventHandler<HTMLInputElement> =  () => {
-    setTodoItems((items) => {
-      if (items) {
-        items[index].done = !items[index].done;
-        console.log('chnage', [...items]);
+const TodoListItem: FunctionComponent<ListItemProps> = ({ item, setTodoItems, index }) => {
+  const handleChange = (item: TodoItem, index: number) => {
+    setTodoItems((prev) => {
+      if (prev) {
+        prev[index].done = !prev[index].done
 
-        return [...items]
+        return [...prev]
       }
 
       return []
@@ -26,41 +21,33 @@ const TodoListItem: FunctionComponent<ListItemProps> = ({
   }
 
   return (
-    <label className="flex cursor-pointer items-center">
-      <input defaultChecked={done} onChange={handleChange} type="checkbox" className="mr-4" />
-      <span className={done ? "line-through opacity-50" : ""}>{text}</span>
-    </label>
-  );
-};
-
-interface TodoListProps {
-  todoItems: TodoItem[];
-  setTodoItems: Dispatch<SetStateAction<TodoItem[] | undefined>>;
+    <li data-index={index}>
+      <label className='flex cursor-pointer items-center'>
+        <input defaultChecked={item.done} onChange={() => handleChange(item, index)} type='checkbox' className='mr-4' />
+        <span className={item.done ? 'line-through opacity-50' : ''}>{item.text}</span>
+      </label>
+    </li>
+  )
 }
 
-const TodoList: FunctionComponent<TodoListProps> = ({
-  todoItems,
-  setTodoItems,
-}) => {
+interface TodoListProps {
+  todoItems: TodoItem[]
+  setTodoItems: Dispatch<SetStateAction<TodoItem[] | undefined>>
+}
+
+const TodoList: FunctionComponent<TodoListProps> = ({ todoItems, setTodoItems }) => {
   return (
-    <div className="group rounded-lg border border-transparent bg-neutral-300/30 px-6 py-4 transition-colors dark:bg-neutral-800/30">
-      {todoItems.length === 0 && (
-        <span className="opacity-50">No items in list</span>
-      )}
+    <div className='group rounded-lg border border-transparent bg-neutral-300/30 px-6 py-4 transition-colors dark:bg-neutral-800/30'>
+      {todoItems.length === 0 && <span className='opacity-50'>No items in list</span>}
       {todoItems.length > 0 && (
-        <div className="space-y-4">
+        <ol className='space-y-4'>
           {todoItems.map((item, index) => (
-            <TodoListItem
-              key={index}
-              index={index}
-              item={item}
-              setTodoItems={setTodoItems}
-            />
+            <TodoListItem key={index} index={index} item={item} setTodoItems={setTodoItems} />
           ))}
-        </div>
+        </ol>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default TodoList;
+export default TodoList
